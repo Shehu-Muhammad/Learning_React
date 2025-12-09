054f7e0ebf5f74d192a533155b1ef9d288ae15c9
Shehu Muhammad <shehuaamuhammad@gmail.com>
2025-12-09 16:24:48 -0500

    rendering message with a constant variable

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -2,18 +2,17 @@ function ListGroup() {
   let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
   items = [];
 
+  const message = items.length === 0 ? <p>No item found</p> : null;
+
   return (
     <>
       <h1>List</h1>
-      {items.length === 0 ? (
-        <p>No item found</p>
-      ) : (
-        <ul className='list-group'>
-          {items.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-      )}
+      {message}
+      <ul className='list-group'>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </>
   );
 }
