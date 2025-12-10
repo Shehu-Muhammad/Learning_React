926a0cf9e840aad17dff14b011bda4e733964a38
Shehu Muhammad <shehuaamuhammad@gmail.com>
2025-12-10 15:23:00 -0500

    created an event handler that handles onClick events

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,5 +1,10 @@
+import type { MouseEvent } from 'react';
+
 function ListGroup() {
   let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
+
+  // Event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
@@ -7,7 +12,7 @@ function ListGroup() {
       {items.length === 0 && <p>No item found</p>}
       <ul className='list-group'>
         {items.map((item, index) => (
-          <li className='list-group-item' key={index}>
+          <li className='list-group-item' key={index} onClick={handleClick}>
             {item}
           </li>
         ))}
