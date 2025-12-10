060d4008c090fafc4a2122fc9d7451d6363604e8
Shehu Muhammad <shehuaamuhammad@gmail.com>
2025-12-10 15:09:24 -0500

    added bootstrap className to list items

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,6 +1,5 @@
 function ListGroup() {
   let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
-  items = [];
 
   return (
     <>
@@ -8,7 +7,9 @@ function ListGroup() {
       {items.length === 0 && <p>No item found</p>}
       <ul className='list-group'>
         {items.map((item, index) => (
-          <li key={index}>{item}</li>
+          <li className='list-group-item' key={index}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
