d7ae8c61ad81a4c858e74447137bc5c3953c9234
Shehu Muhammad <shehuaamuhammad@gmail.com>
2025-12-11 21:01:52 -0500

    alert tutorial

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -3,32 +3,14 @@ import Alert from './components/Alert';
 import Button from './components/Button';
 
 function App() {
-  const [clicked, setClicked] = useState(false);
-  const handleOnClick = () => {
-    setClicked(true);
-  };
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
-    <>
-      <Button onClick={handleOnClick}>Primary</Button>
-      {clicked && (
-        <Alert>
-          <div
-            className='alert alert-primary alert-dismissible fade show'
-            role='alert'
-          >
-            <strong>Holy guacamole!</strong> You should check in on some of
-            those fields below.
-            <button
-              onClick={() => setClicked(false)}
-              type='button'
-              className='btn-close'
-              data-bs-dismiss='alert'
-              aria-label='Close'
-            ></button>
-          </div>
-        </Alert>
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
       )}
-    </>
+      <Button onClick={() => setAlertVisibility(true)}>Primary</Button>
+    </div>
   );
 }
 
--- react-app/src/components/Alert.tsx
+++ react-app/src/components/Alert.tsx
@@ -2,10 +2,25 @@ import type { ReactNode } from 'react';
 
 interface Props {
   children: ReactNode;
+  onClose: () => void;
 }
 
-const Alert = ({ children }: Props) => {
-  return <div>{children}</div>;
+const Alert = ({ children, onClose }: Props) => {
+  return (
+    <div
+      className='alert alert-primary alert-dismissible fade show'
+      role='alert'
+    >
+      {children}
+      <button
+        onClick={onClose}
+        type='button'
+        className='btn-close'
+        data-bs-dismiss='alert'
+        aria-label='Close'
+      ></button>
+    </div>
+  );
 };
 
 export default Alert;
